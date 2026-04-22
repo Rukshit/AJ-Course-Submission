1dc14a80627ca0255393352ac15132962df85386
B Rukshith Naidu <144221895+Rukshit@users.noreply.github.com>
2026-04-23 00:08:27 +0530

    Update RetrieveData.java

--- JDBC/src/main/java/org/example/RetrieveData.java
+++ JDBC/src/main/java/org/example/RetrieveData.java
@@ -7,8 +7,8 @@ public class RetrieveData {
     public static void main(String[] args) {
 
         String url = "jdbc:mysql://localhost:3306/studentdb";
-        String username = "jc";
-        String password = "Jc@25302022";
+        String username = "root";
+        String password = "junnu@14325";
 
         try (Connection con = DriverManager.getConnection(url, username, password);
              Statement stmt = con.createStatement();
